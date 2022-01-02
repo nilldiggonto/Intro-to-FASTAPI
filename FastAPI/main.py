680219a4c7618bc488d967a57a353c6f2505b8e5
nilldiggonto <nilldiggonto@gmail.com>
2022-01-02 14:02:36 +0600

    intro to fastapi

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,12 +1,83 @@
 from fastapi import FastAPI
+from typing import Optional
+
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
+#-----------create request body
+class Blog(BaseModel):
+    title:str
+    body:str
+    publish:Optional[bool]
 
-@app.get('/')
-def index():
+
+#---------blog homepage
+
+@app.get('/blog')
+def index(limit=10,publish:bool = True,sort: Optional[str] = None):
     result = {
-        'data':'FastAPI'
+        'data':'blog home page',
+        'limit':limit,
+        'publish':publish
+    }
+    if publish:
+        result['data']= 'All Publish Block'
+
+    
+    return result
+
+#---------------Create blog
+@app.post('/blog/create')
+def createBlog(request:Blog):
+    # print(request)
+    data = {
+        'status':'success',
+        'msg':'created',
+        'title':request.title
+    }
+    return data
+
+
+
+#unpublish blog
+@app.get('/blog/unpublished')
+def blogUnpublish():
+    result = {
+        'data':'unpublish',
+    }
+    return result
+
+#--- single blog
+@app.get('/blog/{id}')
+def blog(id:int):
+    result = {
+        'data':'blog',
+        'blog_id':id
+    }
+    return result
+
+
+#--- blog comment
+@app.get('/blog/{id}/comments')
+def blogComment(id:int,limit:int =10):
+    result = {
+        'name':'blog',
+        'id':id,
+        'limit':limit,
+        'comments': [
+            {
+                'user':'ok',
+                'comment':'hi',
+                'id':1
+            },
+             {
+                'user':'ok',
+                'comment':'hi',
+                'id':1
+            }
+        ]
     }
     return result
 
@@ -14,7 +85,7 @@ def index():
 @app.get('/about')
 def about():
     result = {
-        'data':'ABOUT PAGE'
+        'data':'blog about page'
     }
     return result
 
@@ -23,3 +94,6 @@ def about():
 """
     uvicorn main:app --reload
 """
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="127.0.0.1",port=9000)
